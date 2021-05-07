a163b3de8860883a4a407b2cdb20ad42fde0610d
fabriciio95 <fabriciusiqueira@gmail.com>
2021-05-07 16:03:31 -0300

    Correção tarefa 4

--- java/AULAS/src/exercicios/lista4/Tarefa4.java
+++ java/AULAS/src/exercicios/lista4/Tarefa4.java
@@ -7,7 +7,7 @@ public class Tarefa4 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+	
 		double[][] matriz1 = new double[2][2];
 		double[][] matriz2 = new double[2][2];
 		double[][] matrizSoma = new double[2][2];
@@ -24,42 +24,46 @@ public class Tarefa4 {
 		char opcaoSaida = 'S';
 		while(opcaoSaida != 'N') {
 			System.out.println("(1) Somar\n(2) Subtrair\n(3) Adicionar valor\n(4) Imprimir");
-			int opcaoMenu = sc.nextInt();
+			char opcaoMenu = sc.next().toUpperCase().charAt(0);
 			
-			if(opcaoMenu == 1) {
+			if(opcaoMenu == '1') {
 				System.out.println("==== MATRIZ SOMA ====");
 				for(int linha = 0; linha < matriz1.length; linha++) {
 					for(int coluna = 0; coluna < matriz1[linha].length; coluna++) {
 						matrizSoma[linha][coluna] = matriz1[linha][coluna] + matriz2[linha][coluna];
-						System.out.printf("%.1f ", matrizSoma[linha][coluna]);
+						System.out.printf("%.0f ", matrizSoma[linha][coluna]);
 					}
 					System.out.println();
 				}
 				System.out.println();
-			} else if(opcaoMenu == 2) {
+			} else if(opcaoMenu == '2') {
 				System.out.println("==== MATRIZ SUBTRAÇÃO ====");
 				for(int linha = 0; linha < matriz1.length; linha++) {
 					for(int coluna = 0; coluna < matriz1[linha].length; coluna++) {
 						matrizSubtracao[linha][coluna] = matriz1[linha][coluna] - matriz2[linha][coluna];
-						System.out.printf("%.1f ", matrizSubtracao[linha][coluna]);
+						System.out.printf("%.0f ", matrizSubtracao[linha][coluna]);
 					}
 					System.out.println();
 				}
 				System.out.println();
-			} else if(opcaoMenu == 3) {
+			} else if(opcaoMenu == '3') {
 				System.out.println("Qual valor deseja adicionar? ");
 				double valor = sc.nextDouble();
-				matriz1[1][1] = valor;
-				matriz2[1][1] = valor;
-			}  else if(opcaoMenu == 4) {
+				for(int linha = 0; linha < matriz1.length; linha++) {
+					for(int coluna = 0; coluna < matriz1[linha].length; coluna++) {
+						matriz1[linha][coluna] += valor;
+						matriz2[linha][coluna] += valor;
+					}
+				}
+			}  else if(opcaoMenu == '4') {
 				System.out.println("==== MATRIZ 1 ====");
 				boolean imprimeMatriz1 = true;
 				for(int linha = 0; linha < matriz1.length; linha++) {
 					for(int coluna = 0; coluna < matriz1[linha].length; coluna++) {
 						if(imprimeMatriz1) {
-							System.out.printf("%.1f ", matriz1[linha][coluna]);
+							System.out.printf("%.0f ", matriz1[linha][coluna]);
 						} else {
-							System.out.printf("%.1f ", matriz2[linha][coluna]);
+							System.out.printf("%.0f ", matriz2[linha][coluna]);
 						}
 					}
 					System.out.println();
